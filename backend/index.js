d4dd0119d2e90e515a1a8a75a0ae248e9a1f2269
Abdullah Tanveer <abdullahkhan1430789@gmail.com>
2025-06-12 16:22:55 +0500

    working on landing page

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
-const connectDB=require("./DB/database.js");
+const connectDB = require("./DB/database.js");
 const cors = require("cors");//different URL pr route krny mey help krta hai
+const axios = require('axios');
 
 require("dotenv").config();// import and configuration of dotenv
 
@@ -18,6 +19,26 @@ app.use(express.json());// Middleware to parse JSON data
 //connection of mongoDB
 connectDB();
 
+
+
+// TMDb Configuration
+const TMDB_API_KEY = 'ffb541e73dda3c12b99b2d15bbd362a8'; // or use Read Access Token in headers
+
+// Route: Get Popular Movies
+app.get('/api/popular-movies', async (req, res) => {
+  try {
+    const response = await axios.get(`https://api.themoviedb.org/3/movie/popular`, {
+      params: {
+        api_key: TMDB_API_KEY
+      }
+    });
+
+    res.json(response.data);
+  } catch (error) {
+    console.error('Error fetching popular movies:', error.message);
+    res.status(500).json({ error: 'Failed to fetch data' });
+  }
+});
 
 const PORT = process.env.PORT || 5000;
 
